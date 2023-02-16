eadb835129058be53c7c399488493641fc7a1b17
agungricky07 <agungricky07@gmail.com>
2023-02-16 12:49:22 +0700

    upload 1 (Coba-coba)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('Lolo', 'welcome');
+
+Route::get('halo', function () {
+    return 'Halo Ricky';
+});
+
+Route::get('product/{id}', function ($id) {
+    return view('landingpage.akun', ['id' => $id]);
+});
+
+
+Route::redirect('about', 'about_sementara');
+
+
+Route::get('/user', [UserController::class, 'index']);
